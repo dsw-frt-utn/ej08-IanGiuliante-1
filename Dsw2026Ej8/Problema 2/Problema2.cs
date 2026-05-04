662b5a65d926d2a23b8090cc10aae9367154734b
Ian Giuliante <ian.giuliante@gmail.com>
2026-05-03 21:11:32 -0300

    resolucion de problema 2

--- Dsw2026Ej8/Problema 2/Problema2.cs
+++ Dsw2026Ej8/Problema 2/Problema2.cs
@@ -4,7 +4,22 @@ using System.Text;
 
 namespace Dsw2026Ej8.Problema_2
 {
-    internal class Problema2
+    public class Problema2
     {
+        public string CrearResumenVenta(long productCode, string productDescription, int quantity, decimal unitPrice) {
+            decimal total=0;
+
+            if (quantity>0) {
+                total= quantity*unitPrice;
+            }
+            else {
+                total = 0;
+            }
+
+            var venta = new { Code = productCode, Description = productDescription, Quantity = quantity, Total = total };
+
+            return $"{venta.Code}-{venta.Description}-{venta.Total}";
+        }
+
     }
 }
