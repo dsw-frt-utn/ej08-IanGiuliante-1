895c29f30916fb3f8c44202c22c09b15a14a9d6e
Ian Giuliante <ian.giuliante@gmail.com>
2026-05-03 21:51:59 -0300

    resolucion de problema 5

---
+++ Dsw2026Ej8/Problema 5/Sale.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.Problema_5
+{
+    public class Sale
+    {
+        public decimal Importe { get; set; }
+        public virtual decimal CalculateTotal() {
+            return Importe;            
+        }
+    }
+}
