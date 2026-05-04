160d5a3ebd4f23d54fec1e01ef4691f8723fb6da
Ian Giuliante <ian.giuliante@gmail.com>
2026-05-03 21:21:27 -0300

    resolucion de problema 3

--- Dsw2026Ej8/Problema 3/Problema3.cs
+++ Dsw2026Ej8/Problema 3/Problema3.cs
@@ -4,7 +4,17 @@ using System.Text;
 
 namespace Dsw2026Ej8.Problema_3
 {
-    internal class Problema3
+    public class Problema3
     {
+        public string CompararCopiar(int originalValue, Product product) { 
+            int originalValueLocal=originalValue;
+            Product productLocal = product;
+            
+            originalValueLocal += 1;
+
+            productLocal.SetDescription("Nueva descripcion del producto!");
+
+            return $"{originalValue}-{originalValueLocal}-{productLocal._description}";
+        }
     }
 }
