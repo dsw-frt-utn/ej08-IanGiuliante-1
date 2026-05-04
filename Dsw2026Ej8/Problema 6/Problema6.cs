9c213f1ef130897da631a2584559b3df0807c9b1
Ian Giuliante <ian.giuliante@gmail.com>
2026-05-03 22:08:59 -0300

    resolucion de problema 6

---
+++ Dsw2026Ej8/Problema 6/Problema6.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.Problema_6
+{
+    public class Problema6
+    {
+        public string NormalizarCodigoProducto(string code) {
+            string message;
+            message=code.ToProductCode();
+
+            return message;
+        }
+    }
+}
